892e280cd5947672326ca0e544995312532b5cfb
Fabian Sohrmann <mfsohr@utu.fi>
2021-04-03 11:18:22 +0300

    Initial commit

---
+++ src/main/java/snake/Game.java
@@ -0,0 +1,14 @@
+package snake;
+
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public class Game extends Application {
+	
+	@Override
+    public void start(Stage stage) {
+		stage.setTitle("Testrun");
+		stage.show();
+	}
+	
+}
---
+++ src/main/java/snake/Main.java
@@ -0,0 +1,12 @@
+package snake;
+
+import javafx.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Application.launch(Game.class, args);
+	}
+
+}
