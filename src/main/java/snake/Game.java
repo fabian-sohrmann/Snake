bb009c143b8ab610d216d44dfc8f489d93e9ee1e
Fabian Sohrmann <mfsohr@utu.fi>
2021-04-06 13:07:44 +0300

    Successfully added FXML file to project

--- src/main/java/snake/Game.java
+++ src/main/java/snake/Game.java
@@ -1,12 +1,28 @@
 package snake;
 
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
 public class Game extends Application {
 	
 	@Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws IOException {
+		
+		/**
+		 * Loading the FXML file
+		 */
+		String form = "snake.fxml";
+		FXMLLoader loader = new FXMLLoader(getClass().getResource(form));
+		Parent root = loader.load();
+		
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
 		stage.setTitle("Testrun");
 		stage.show();
 	}
