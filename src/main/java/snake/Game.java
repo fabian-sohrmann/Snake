d20ae1073e4b628f1ec0174b021466ae6a3befb5
Fabian Sohrmann <mfsohr@utu.fi>
2021-04-08 12:30:33 +0300

    Gave up on fxml, doing just with javafx

--- src/main/java/snake/Game.java
+++ src/main/java/snake/Game.java
@@ -3,28 +3,47 @@ package snake;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 
 public class Game extends Application {
 	
 	@Override
     public void start(Stage stage) throws IOException {
 		
-		/**
-		 * Loading the FXML file
-		 */
-		String form = "snake.fxml";
-		FXMLLoader loader = new FXMLLoader(getClass().getResource(form));
-		Parent root = loader.load();
 		
-		Scene scene = new Scene(root);
+		
+		
+		
+		
+		
+		
+		
+		//graphics start here
+		Button startButton = new Button("Start");
+		Label title = new Label("Snake");
+		BorderPane pane = new BorderPane();
+		pane.setTop(title);
+		pane.setBottom(startButton);
+		
+		
+		Scene scene = new Scene(pane);
 		stage.setScene(scene);
 		stage.setTitle("Testrun");
 		stage.show();
+		
+	}
+	
+	public void startGame() {
+		Kentta kentta = new Kentta(10, 10);
+		
 	}
 	
 }
--- src/main/java/snake/Mato.java
+++ src/main/java/snake/Mato.java
@@ -9,15 +9,18 @@ public class Mato {
 	 */
 	private ArrayList<Pala> keho = new ArrayList<Pala>();
 	
+	//vaihtoehdot: ylos, alas, oikea, vasen
 	String headDir;
 	String tailDir;
 	
 	
 	/**
-	 * 
+	 * Konstruktori lis‰‰ ja suunnan madon keholle
 	 */
 	public Mato() {
-		
+		Pala paa = new Pala(5,5);
+		keho.add(paa);
+		headDir = "vasen";
 	}
 	
 	public ArrayList<Pala> getKeho(){
@@ -25,42 +28,59 @@ public class Mato {
 	}
 	
 	
-	public void setDirection(String dir) {
-		String direction = dir;
-	}
-	
-	
 	public void grow() {
 		int x = 0;
 		int y = 0;
 		
-		//haetaan 2 viimeisint‰ palaa, verrataan niiden kulkusuunta
-		//ja kasvatetaan h‰nt‰ menosuunnasta poisp‰in
-		Pala vikaPala = keho.get(keho.size()-1);
-		Pala vertPala = keho.get(keho.size()-2); 		
-		int x2 = vertPala.getX();
-		int y2 = vertPala.getY();
-		int x1 = vikaPala.getX();
-		int y1 = vikaPala.getY();
 		
-		if(x1>x2 ) {
-			x = x1 + 1;
-			y = y1;
-		}
-		if(x1<x2 ) {
-			x = x1 - 1;
-			y = y1;
-		}
-		if(y1<y2 ) {
-			x = x1;
-			y = y1 + 1;
-		}
-		if(y1>y2) {
-			x = x1;
-			y = y1-1;
-		}
-		
+		if(keho.size() == 1) {
+			if(headDir.equals("vasen")) {
+				x = keho.get(0).getX();
+				y = keho.get(0).getY() + 1;
+			}
+			if(headDir.equals("oikea")) {
+				x = keho.get(0).getX();
+				y = keho.get(0).getY() - 1;
+			}
+			if(headDir.equals("ylos")) {
+				x = keho.get(0).getX() + 1;
+				y = keho.get(0).getY();
+			}
+			if(headDir.equals("alas")) {
+				x = keho.get(0).getX() - 1;
+				y = keho.get(0).getY();
+			}
+		}else{
+			//haetaan 2 viimeisint‰ palaa, verrataan niiden kulkusuunta
+			//ja kasvatetaan h‰nt‰ menosuunnasta poisp‰in
+			Pala vikaPala = keho.get(keho.size()-1);
+			Pala vertPala = keho.get(keho.size()-2); 		
+			int x2 = vertPala.getX();
+			int y2 = vertPala.getY();
+			int x1 = vikaPala.getX();
+			int y1 = vikaPala.getY();
+			
+			if(x1>x2 ) {
+				x = x1 + 1;
+				y = y1;
+			}
+			if(x1<x2 ) {
+				x = x1 - 1;
+				y = y1;
+			}
+			if(y1<y2 ) {
+				x = x1;
+				y = y1 + 1;
+			}
+			if(y1>y2) {
+				x = x1;
+				y = y1-1;
+			}
+			
 		//lis‰t‰‰n oikeat koordinaatit uudelle h‰nn‰lle ja lis‰t‰‰n uusi h‰nt‰ listaan
 		keho.add(keho.size(), new Pala(x, y));
+		}	
 	}
+	
+	
 }
